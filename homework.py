2042b42f93a7b6459ffaa6b6b4fa3f09b7244a8f
Александр Солдатов <p90_master@mail.ru>
2021-11-09 01:37:35 +0300

    hw06 ver1.1

--- homework.py
+++ homework.py
@@ -6,6 +6,8 @@ import logging
 
 from telegram import Bot
 from dotenv import load_dotenv
+from http import HTTPStatus
+
 
 load_dotenv()
 
@@ -19,7 +21,7 @@ RETRY_TIME = 300
 HEADERS = {'Authorization': f'OAuth {ENV_VAR.get("PRACTICUM_TOKEN")}'}
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 
-HOMEWORK_STATUSES = {
+VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена, в ней нашлись ошибки.'
@@ -57,29 +59,34 @@ class CriticalException(Exception):
 
 def send_message(bot, message):
     """Отправка сообщения выбранным ботом фиксированному пользователю."""
+    user_id = ENV_VAR.get('TELEGRAM_CHAT_ID')
+
     try:
         bot.send_message(
-            chat_id=ENV_VAR.get('TELEGRAM_CHAT_ID'),
+            chat_id=user_id,
             text=message
         )
-        logging.info('Сообщение пользователю успешно отправлено')
+        logging.info(f'Сообщение пользователю {user_id} успешно отправлено')
 
     except Exception:
-        logging.error('Ошибка при отправке сообщения пользователю')
+        logging.error(f'Ошибка при отправке сообщения пользователю {user_id}')
 
 
 def get_api_answer(url, current_timestamp):
     """Получение HTTP-ответа от API-сервиса."""
-    payload = {'from_date': current_timestamp}
+    try:
+        payload = {'from_date': current_timestamp}
+        practicum_response = requests.get(
+            url, headers=HEADERS, params=payload
+        )
 
-    practicum_response = requests.get(
-        url, headers=HEADERS, params=payload
-    )
+        if practicum_response.status_code == HTTPStatus.OK.value:
+            return practicum_response.json()
 
-    if practicum_response.status_code == 200:
-        return practicum_response.json()
+        raise ErrorException('Неудачный HTTP-запрос')
 
-    raise ErrorException('Неудачный HTTP-запрос')
+    except Exception:
+        raise ErrorException('Неудачный HTTP-запрос')
 
 
 def parse_status(homework):
@@ -87,30 +94,43 @@ def parse_status(homework):
     verdict = homework.get('status')
     homework_name = homework.get('homework_name')
 
-    message = (
-        f'Изменился статус проверки работы "{homework_name}". ',
-        f'{HOMEWORK_STATUSES.get(verdict)}'
-    )
+    if (verdict or homework_name) is None:
+        raise ErrorException('Нехватка данных в API-ответе')
 
-    return message[0] + message[1]
+    verdict = VERDICTS.get(verdict)
+
+    # Неизвестные статусы отсеиваются в check_response
+    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_response(response):
     """Проверка полученного API-ответа на корректность."""
     homework = response.get('homeworks')
 
-    if homework is not None:
-        if len(homework) > 0:
-            homework = homework[0]
+    if homework is None:
+        raise ErrorException('API-ответ пуст')
 
-            if homework.get('status') in HOMEWORK_STATUSES.keys():
-                return homework
-
-            raise ErrorException('Статус работы в API-ответе не распознан')
-
+    if len(homework) == 0:
         raise DebugException('Нет обновлений')
 
-    raise ErrorException('API-ответ пуст')
+    homework = homework[0]
+
+    if homework.get('status') not in VERDICTS.keys():
+        raise ErrorException('Статус работы в API-ответе не распознан')
+
+    return homework
+
+
+def env_check():
+    """Проверка наличия всех переменных окружения."""
+    for var in ENV_VAR.keys():
+        if ENV_VAR.get(var) is None:
+            message = (
+                f'Критическая ошибка: отсутствует переменная окружения {var}'
+            )
+
+            logging.critical(message)
+            exit()
 
 
 def main():
@@ -120,24 +140,14 @@ def main():
         - Анализирует полученные ответы;
         - Пересылает статус домашней работы выбранному telegram-пользователю;
     """
-    logging.info('Начало работы..')
-
-    for var in ENV_VAR.keys():
-        if ENV_VAR.get(var) is None:
-            message = (
-                f'Критическая ошибка: отсутствует переменная окружения {var}')
-
-            logging.critical(message)
-            return
-    logging.info('Все переменные окружения на месте')
+    env_check()
 
     try:
         bot = Bot(token=ENV_VAR.get("TELEGRAM_TOKEN"))
-        logging.info('Объект-бот успешно инициализирован')
 
     except Exception:
         logging.critical('Ошибка при создании объекта бота')
-        return
+        exit()
 
     current_timestamp = int(time.time())
 
@@ -156,6 +166,7 @@ def main():
         except ErrorException as error:
             message = f'Сбой в работе программы: {error}'
             logging.error(message)
+            send_message(bot, message)
 
         except CriticalException as error:
             message = f'Критическая ошибка: {error}'
